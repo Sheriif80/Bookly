68b6e38eab9f646b4cb2f02d64740579394eed7d
Sherif80 <sherifashraf1054@gmail.com>
2026-01-15 14:08:58 +0200

    finished splash view ui

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,12 @@
+import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Center(child: SvgPicture.asset(AssetsData.logo, width: 230));
   }
 }
