975c9d67e19f02162f0a5a1ea45c3d3b644442fb
Sherif80 <sherifashraf1054@gmail.com>
2026-01-15 16:14:09 +0200

    added custom app bar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:bookly/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+      child: Row(
+        children: [
+          SvgPicture.asset(AssetsData.logo),
+          const Spacer(),
+          const Icon(FontAwesomeIcons.magnifyingGlass),
+        ],
+      ),
+    );
+  }
+}
