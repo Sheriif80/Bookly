7a6a210a38b5eda4c4fe8be44c83366b91041426
Sherif80 <sherifashraf1054@gmail.com>
2026-01-19 22:12:58 +0200

    added the search view and finished its UI

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,7 +1,9 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -15,7 +17,9 @@ class CustomAppBar extends StatelessWidget {
           SvgPicture.asset(AssetsData.logo, width: 75, height: 19),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.searchView);
+            },
             icon: const Icon(
               FontAwesomeIcons.magnifyingGlass,
               size: 25,
