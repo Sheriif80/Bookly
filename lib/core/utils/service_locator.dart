ac27ef3e6ac84ff0f83103d2b76f333f1163ba44
Sherif80 <sherifashraf1054@gmail.com>
2026-01-21 17:25:36 +0200

    Added the GetIT Package to use it for the API Service and created a service locator file for ir

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementaion.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+
+  getIt.registerSingleton<HomeRepoImplementaion>(
+    HomeRepoImplementaion(apiService: getIt.get<ApiService>()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_implementaion.dart';
+import 'package:bookly/features/home/presentation/manager/featured%20books%20cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/newest%20books%20cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,16 +20,34 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
-        ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(getIt.get<HomeRepoImplementaion>());
+          },
+        ),
+        // I can use GetIT package here and write the code as the same as above,
+        // but I prefer to write it like this to show the diffrence between
+        // the two lines and the dependency injection pattern importance.
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            HomeRepoImplementaion(apiService: ApiService(Dio())),
+          ),
+        ),
+      ],
+
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
