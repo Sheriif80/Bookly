8c34ca88e897bd06cd932b7091baba755eaa6103
Sherif80 <sherifashraf1054@gmail.com>
2026-01-21 23:10:58 +0200

    added th bloc provider and bloc builder and fixed some typre problems with the models and added observer for the app

--- lib/features/home/presentation/views/widgets/books_listview_builder.dart
+++ lib/features/home/presentation/views/widgets/books_listview_builder.dart
@@ -1,22 +1,35 @@
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/features/home/presentation/manager/featured%20books%20cubit/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BooksListviewBuilder extends StatelessWidget {
   const BooksListviewBuilder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSucess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.25,
 
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: .horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return const BookItem();
-        },
-      ),
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: .horizontal,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return const BookItem();
+              },
+            ),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errorMessage: state.errMessage);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/observer.dart';
 import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/features/home/data/repos/home_repo_implementaion.dart';
 import 'package:bookly/features/home/presentation/manager/featured%20books%20cubit/featured_books_cubit.dart';
@@ -11,6 +12,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   setupServiceLocator();
   runApp(const BooklyApp());
 }
@@ -22,9 +24,10 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
+        BlocProvider<FeaturedBooksCubit>(
           create: (context) {
-            return FeaturedBooksCubit(getIt.get<HomeRepoImplementaion>());
+            return FeaturedBooksCubit(getIt.get<HomeRepoImplementaion>())
+              ..getFeaturedBooks();
           },
         ),
         // I can use GetIT package here and write the code as the same as above,
